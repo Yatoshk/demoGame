ed22e1cf7905102a45b6adfaf8a6e6e2aeed8adc
Yatoshk <48162785+Yatoshk@users.noreply.github.com>
2022-09-11 14:43:12 +0400

    base

---
+++ src/main/java/com/example/demoGame/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demoGame.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World")
+                                       String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/test/java/com/example/demoGame/DemoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
